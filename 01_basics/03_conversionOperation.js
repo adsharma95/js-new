21b043bffe3e9b1236607c50b6f03011822464d0
adsharma95 <adsharma95@gmail.com>
2024-01-25 14:26:04 +0000

    Conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = "40a"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+console.log(valueInNumber);
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 30
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
