2e93d7dcd5518586b4a2c90d33c96d847fca2146
adsharma95 <adsharma95@gmail.com>
2024-01-25 18:36:57 +0000

    Conversion operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,16 +1,27 @@
 let score = "40a"
 
-console.log(typeof score)
+// console.log(typeof score)
 
 let valueInNumber = Number(score)
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 let someNumber = 30
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************************************** Operations ******************************************* 
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Adhar"
+let str3 = str1+str2;
+console.log(str3);
